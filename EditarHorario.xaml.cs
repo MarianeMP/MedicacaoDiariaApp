473962c833e35a2c95988beec75ee70a43e020de
João Prado <114949065+joaonano@users.noreply.github.com>
2025-05-04 10:32:08 -0300

    Cadastro novo horário e dose do medicamento(2)

--- EditarHorario.xaml.cs
+++ EditarHorario.xaml.cs
@@ -1,9 +1,47 @@
-namespace MedicacaoDiariaApp.Views;
+using Microsoft.Maui.Controls;
+using System;
+using MedicacaoDiariaApp.Helpers;
+using MedicacaoDiariaApp.Models;
+using System.IO;
 
-public partial class EditarHorario : ContentPage
+namespace MedicacaoDiariaApp.Views
 {
-	public EditarHorario()
-	{
-		InitializeComponent();
-	}
+    public partial class EditarHorario : ContentPage
+    {
+        private SQLiteBancoDeDados _bancoDeDados;
+        private HorarioMedicamento _horarioMedicamento;
+
+        public EditarHorario(HorarioMedicamento horarioMedicamento)
+        {
+            InitializeComponent();
+
+
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "medicacaodiaria.db3");
+            _bancoDeDados = new SQLiteBancoDeDados(dbPath);
+
+            _horarioMedicamento = horarioMedicamento;
+
+          
+            MedicamentoEntry.Text = _horarioMedicamento.IdMedicamento.ToString();
+            DosagemEntry.Text = _horarioMedicamento.Dosagem.ToString();
+            HorarioTimePicker.Time = _horarioMedicamento.Horario.TimeOfDay;
+        }
+
+        
+        private async void OnSalvarAlteracoesClicked(object sender, EventArgs e)
+        {
+            
+            _horarioMedicamento.Dosagem = Convert.ToDouble(DosagemEntry.Text);
+            _horarioMedicamento.Horario = DateTime.Today.Add(HorarioTimePicker.Time); 
+
+            
+            await _bancoDeDados.EditarHorarioMedicamento(_horarioMedicamento);
+
+            
+            await DisplayAlert("Sucesso", "Horário de medicação atualizado!", "OK");
+
+            
+            await Navigation.PopAsync();
+        }
+    }
 }
